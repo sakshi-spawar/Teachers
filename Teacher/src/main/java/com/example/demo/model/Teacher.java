d347fc96a91d182e2fa270342b775153a46f25a2
sakshi pawar <sakshipawar2336@gmail.com>
2025-12-10 14:30:22 +0530

    upgrade new

--- Teacher/src/main/java/com/example/demo/model/Teacher.java
+++ Teacher/src/main/java/com/example/demo/model/Teacher.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +9,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Teacher {
 	
+	@Id
 	private Integer id;
 	private String name;
 	private Float salary;
